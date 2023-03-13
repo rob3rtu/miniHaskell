9ca2af38256f19dd75ec142fbe77db5457d32ceb
rob3rtu <robertudrea2002@yahoo.ro>
2023-03-13 15:37:59 +0200

    running lab3 good

--- app/Main.hs
+++ app/Main.hs
@@ -8,7 +8,18 @@ import Exp
 import Parsing
 import Printing
 import REPLCommand
+import Text.ParserCombinators.Parsec 
 
 main :: IO ()
-main = undefined
+main = do
+    setHistory "history.txt" 200
+    input <- readline "minHaskell"
+    case parse replCommand "<input>" input of
+        Left err -> print err >> main
+        Right cmd -> case cmd of
+                        Quit -> return ()
+                        Load _ -> putStrLn("Not implemented") >> main
+                        Eval s -> case parse exprParser "<input>" s of
+                            Left err -> print err >> main
+                            Right c -> putStrLn (showExp c) >> main
 
--- src/REPLCommand.hs
+++ src/REPLCommand.hs
@@ -5,12 +5,13 @@ import Text.Parsec.String (Parser)
 import Text.Parsec.Language (emptyDef, LanguageDef)
 import Text.Parsec.Token
 import Text.Parsec (anyChar)
-import Control.Applicative (many, (<|>))
+import Control.Applicative (many, some, (<|>))
 
 data REPLCommand
   = Quit
   | Load String
   | Eval String
+  deriving (Show)
 
 replDef :: LanguageDef st
 replDef = emptyDef {
@@ -24,6 +25,15 @@ repl = makeTokenParser replDef
 replQuit :: Parser REPLCommand
 replQuit = pure Quit <$> (((reservedOp repl) ":q") <|> ((reserved repl) ":quit"))
 
+replLoad :: Parser REPLCommand
+replLoad = do 
+  (((reservedOp repl) ":l") <|> ((reserved repl) ":load"))
+  s <- some anyChar
+  return $ Load s
+
+replEval :: Parser REPLCommand
+replEval = Eval <$> many anyChar
+
 replCommand :: Parser REPLCommand
-replCommand = undefined
+replCommand = replQuit <|> replLoad <|> replEval
 
